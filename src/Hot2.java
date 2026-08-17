3189d05e08b19d7204ba6e68113ffb617dd24f1f
Yearn <q2681910429@gmail.com>
2026-08-18 00:28:22 +0800

    力扣hot100，第二题

---
+++ src/Hot2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Hot2 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(s);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+
+    }
+    public static void main(String[] args) {
+        Hot2 hot2 = new Hot2();
+        List<List<String>> lists = hot2.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        System.out.println(lists);
+    }
+}
