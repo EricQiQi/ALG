36015f7015cd92337cb79e699b3e09ed522867a3
Github <q2681910429@gmail.com>
2026-09-03 17:17:44 +0800

    代码提交

--- src/s8_tree/Hot114_flatten.java
+++ src/s8_tree/Hot114_flatten.java
@@ -15,7 +15,7 @@ public class Hot114_flatten {
      * 空间复杂度：O(n)
      * @param root
      */
-    public static void flatten(TreeNode root) {
+    public static void flatten_1(TreeNode root) {
         List<TreeNode> res = new ArrayList<>();
         preOrder(root, res);
 
@@ -34,6 +34,34 @@ public class Hot114_flatten {
         preOrder(node.right, res);
     }
 
+
+    /**
+     * 方法2：迭代-寻找前驱节点
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     * @param root
+     */
+    public static void flatten_2(TreeNode root) {
+        TreeNode curr = root;
+        while(curr != null){
+            if (curr.left != null){
+                // 找到左子树的最右节点
+                TreeNode next = curr.left;
+                TreeNode preNode = next;
+                while(preNode.right != null){
+                    preNode = preNode.right;
+                }
+                // 将 curr 的右子树接到 preNode 的右子树
+                preNode.right = curr.right;
+                // 将 curr 的左子树接到 curr 的右子树
+                curr.left = null;
+                curr.right = next;
+            }
+            // 移动到右子树
+            curr = curr.right;
+        }
+    }
+
     public static void main(String[] args) {
         //         1
         //        / \
@@ -44,7 +72,8 @@ public class Hot114_flatten {
                 new TreeNode(2, new TreeNode(3), new TreeNode(4)),
                 new TreeNode(5, null, new TreeNode(6)));
 
-        new Hot114_flatten().flatten(root);
+//        new Hot114_flatten().flatten_1(root);
+        new Hot114_flatten().flatten_2(root);
 
         // 展开后沿 right 遍历：1 -> 2 -> 3 -> 4 -> 5 -> 6
         StringBuilder sb = new StringBuilder();
